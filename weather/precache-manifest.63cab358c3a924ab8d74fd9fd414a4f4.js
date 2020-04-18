8caa9857774254915dc5ad9a0c4dd82abb6ed633
AlexSuboch <53902207+AlexSuboch@users.noreply.github.com>
2020-04-19 00:11:11 +0300

    added weather app

---
+++ weather/precache-manifest.63cab358c3a924ab8d74fd9fd414a4f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9713dd7c9cde76e43f624368368c4f72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d68fc149a2806d69bd4",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "db51a579208d9750affa",
+    "url": "/static/js/2.81384bb0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.81384bb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d68fc149a2806d69bd4",
+    "url": "/static/js/main.f97a6d6a.chunk.js"
+  },
+  {
+    "revision": "228fa098814da32aa5c2",
+    "url": "/static/js/runtime-main.e1daef6b.js"
+  }
+]);
