a554488ae6ba12000e0fbaf68ae4f41492adc175
AlexSuboch <53902207+AlexSuboch@users.noreply.github.com>
2020-11-09 20:07:25 +0300

    reactBar added

---
+++ reactBar/precache-manifest.639b2556a93d2577df6ac0c1cf6f5450.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf32c9c52d6d1cf8ac0b82fddfae8611",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6758f89865c1883f6cf",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "791ebdb56eb0b804e2a0",
+    "url": "/static/js/2.c2af8ee5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.c2af8ee5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6758f89865c1883f6cf",
+    "url": "/static/js/main.9bb82fe4.chunk.js"
+  },
+  {
+    "revision": "b8f7d3778dc9fbcee248",
+    "url": "/static/js/runtime-main.de4647a3.js"
+  }
+]);
